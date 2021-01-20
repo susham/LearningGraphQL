fb5b82e8dad6538e2db3512f18c7ea379c2c6293
susham <susham.kumar@gmail.com>
2021-01-19 20:49:55 -0600

    added relation between Users and Posts types and capability to return array of posts for each user

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -5,18 +5,43 @@ const users = [{
     id:'1',
     name: "Susham",
     email:"test@test.com"
+    },
+    {
+    id:'2',
+    name: "SYerabolu",
+    email:"test@test1.com"
+    }
+]
+
+const posts = [{
+
+id:'10',
+title: 'GraphQL 101',
+body: 'This is how to use GraphQL',
+published:true,
+author:'1'
 },
 {
-id:'2',
-name: "SYerabolu",
-email:"test@test1.com"
-}
-]
+    id:'11',
+    title: 'GraphQL 201',
+    body: 'This is an advanced GraphQL post',
+    published:false,
+    author:'1'
+},
+{    
+    id:'12',
+    title: 'Programming Music',
+    body: 'This is how to use GraphQL',
+    published:false,
+    author:'2'
+}]
 
 const typeDefs = `
 type Query {
    users(query: String):[User!]!
+   posts: [Post!]!
     me: User!
+    post:Post!
   
     }
 
@@ -25,6 +50,15 @@ type Query {
         name: String!
         email: String!
         age: Int
+        posts: [Post!]!
+    }
+
+    type Post{
+        id: ID!
+        title: String!
+        body: String!
+        published:Boolean!
+        author: User!
     }
 `
 
@@ -51,6 +85,27 @@ const resolvers = {
                
 
             }
+        },
+
+        posts(){
+            return posts
+        }
+    },
+    Post:{
+        author(parent, args, ctx, info){
+            return users.find((user) => {
+                console.log(user.id)
+                return user.id === parent.author
+
+            })
+        }
+    },
+    User:{
+        posts(parent, args, ctx, info){
+            return posts.filter((post) => {
+                return post.author === parent.id
+
+            })
         }
     }
 }
